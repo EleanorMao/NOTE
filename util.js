a0dbce9642703590864f0fbda13f228963785521
EleanorMao <danningmao@outlook.com>
2017-06-01 14:22:09 +0800

    Update util.js

--- util.js
+++ util.js
@@ -194,4 +194,73 @@ function ellipsis(selector, innerSelector) {
     })
 }
 
-var requestAnimationFrame = window.requestAnimationFrame ||
+var requestAnimationFrame = window.requestAnimationFrame ||  window.webkitRequestAnimationFrame ||
+    function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+    
+function getElementTop(element) {
+    var actualTop = element.offsetTop;
+    var current = element.offsetParent;
+
+    while (current !== null) {
+        actualTop += current.offsetTop;
+        current = current.offsetParent;
+    }
+
+    return actualTop;
+}
+
+function getElementLeft(element) {
+    var actualLeft = element.offsetLeft;
+    var current = element.offsetParent;
+
+    while (current !== null) {
+        actualLeft += current.offsetLeft;
+        current = current.offsetParent;
+    }
+
+    return actualLeft;
+}
+
+function getBoundingClientRect(element) {
+    var scrollTop = document.documentElement.scrollTop;
+    var scrollLeft = document.documentElement.scrollLeft;
+
+    if (element.getBoundingClientRect) {
+        if (typeof arguments.callee.offset != "number") {
+            var temp = document.createElement("div");
+            temp.style.cssText = "position: absolute;left: 0;top: 0;"
+            document
+                .body
+                .appendChild(temp);
+            arguments.callee.offset = -temp
+                .getBoundingClientRect()
+                .top - scrollTop;
+            document
+                .body
+                .removeChild(temp);
+            temp = null;
+        }
+
+        var rect = element.getBoundingClientRect();
+        var offset = arguments.callee.offset;
+
+        return {
+            left: rect.left + offset,
+            right: rect.right + offset,
+            top: rect.top + offset,
+            bottom: rect.bottom + offset
+        }
+    } else {
+        var actualTop = getElementTop(element);
+        var actualLeft = getElementLeft(element);
+
+        return {
+            left: actualLeft - scrollLeft,
+            right: actualLeft + element.offsetWidth - scrollLeft,
+            top: actualTop - screenTop,
+            bottom: actualTop + element.offsetHeight - scrollTop
+        }
+    }
+}
