4cdb9fba166909577bbf209e9d47016bb7cbc621
EleanorMao <danningmao@outlook.com>
2017-06-02 11:23:15 +0800

    Update util.js

--- util.js
+++ util.js
@@ -1,21 +1,28 @@
 //是不是ie
 function isIE() {
-    return !-[1,];
+    return ! - [1];
 }
 
 //判断是否是object类型
 function isObject(input) {
-    return Object.prototype.toString.apply(input) === "[object Object]"
+    return Object
+        .prototype
+        .toString
+        .apply(input) === "[object Object]"
 }
 
 //判断是否是Array类型
 function isArray(input) {
-    return Object.prototype.toString.apply(input) === "[object Array]"
+    return Object
+        .prototype
+        .toString
+        .apply(input) === "[object Array]"
 }
 
 //判断el是否包含在root中
 function contains(root, el) {
-    if (!root || !el) return false;
+    if (!root || !el) 
+        return false;
     if (root.compareDocumentPosition) {
         return root === el || !!(root.compareDocumentPosition(el) & 16);
     }
@@ -23,8 +30,9 @@ function contains(root, el) {
         return root.contains(el) && root !== el;
     }
     while (el = el.parentNode) {
-        if (el === root) return true;
-    }
+        if (el === root) 
+            return true;
+        }
     return false;
 }
 
@@ -41,10 +49,13 @@ function defaults(defs, obj) {
 //拷贝，最后加上true代表深拷贝
 function assign(target) {
     var length = arguments.length;
-    var deep = typeof arguments[length - 1] === "boolean" ? arguments[length - 1] : false;
+    var deep = typeof arguments[length - 1] === "boolean"
+        ? arguments[length - 1]
+        : false;
     for (var i = 1; i < length; i++) {
         var source = arguments[i];
-        if (!isObject(source)) continue;
+        if (!isObject(source)) 
+            continue;
         for (var key in source) {
             if (Object.prototype.hasOwnProperty.call(source, key)) {
                 var value = source[key];
@@ -69,7 +80,9 @@ function diff(arr1, arr2) {
 function compact(array) {
     var output = [];
     array.forEach(function (i) {
-        if (i) { output.push(i); }
+        if (i) {
+            output.push(i);
+        }
     })
     return output;
 }
@@ -90,12 +103,14 @@ function unique(array) {
 
 //打平数组
 function flatten(array) {
-    var output = [], index = 0;;
+    var output = [],
+        index = 0;;
     for (let i = 0; i < array.length; i++) {
         var value = array[i];
         if (isArray(value)) {
             value = flatten(value);
-            var j = 0, len = value.length;
+            var j = 0,
+                len = value.length;
             output.length += len;
             while (j < len) {
                 output[index++] = value[j++];
@@ -109,7 +124,9 @@ function flatten(array) {
 
 //去除字符串前后的空格
 function trim(str) {
-    return str == null ? "" : (str + '').replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
+    return str == null
+        ? ""
+        : (str + '').replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
 }
 
 //绑定事件兼容
@@ -135,14 +152,18 @@ function delegateEvent(parent, childTag, ev, listener) {
 
 //插入后面
 function insertAfter(newNode, referenceNode) {
-    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    referenceNode
+        .parentNode
+        .insertBefore(newNode, referenceNode.nextSibling);
 }
 
 //增加class
 function addClass(el, className) {
     var oldClass = el.className;
-    if (!~oldClass.indexOf(className)) {
-        el.className = oldClass === "" ? className : oldClass + " " + className;
+    if (!~ oldClass.indexOf(className)) {
+        el.className = oldClass === ""
+            ? className
+            : oldClass + " " + className;
     }
 }
 
@@ -169,36 +190,43 @@ function getScrollBarWidth() {
     outer.style.overflow = 'hidden';
     outer.appendChild(inner);
 
-    document.body.appendChild(outer);
+    document
+        .body
+        .appendChild(outer);
     var w1 = inner.offsetWidth;
     outer.style.overflow = 'scroll';
     var w2 = inner.offsetWidth;
-    if (w1 === w2) w2 = outer.clientWidth;
-
-    document.body.removeChild(outer);
+    if (w1 === w2) 
+        w2 = outer.clientWidth;
+    
+    document
+        .body
+        .removeChild(outer);
 
     return w1 - w2;
 };
 
 //超长加省略号
 function ellipsis(selector, innerSelector) {
-    document.querySelectorAll(selector).forEach(function (item) {
-        let outerHeight = item.offsetHeight;
-        let innerText = item.querySelector(innerSelector);
-        if (!innerText) return;
-        var times = 20;
-        while (innerText.offsetHeight > outerHeight && times--) {
-            let text = innerText.innerText;
-            innerText.innerText = text.replace(/([\S\s])\.\.\.$|[\S\s]$/, '...');
-        }
-    })
+    document
+        .querySelectorAll(selector)
+        .forEach(function (item) {
+            let outerHeight = item.offsetHeight;
+            let innerText = item.querySelector(innerSelector);
+            if (!innerText) 
+                return;
+            var times = 20;
+            while (innerText.offsetHeight > outerHeight && times--) {
+                let text = innerText.innerText;
+                innerText.innerText = text.replace(/([\S\s])\.\.\.$|[\S\s]$/, '...');
+            }
+        })
 }
 
-var requestAnimationFrame = window.requestAnimationFrame ||  window.webkitRequestAnimationFrame ||
-    function (callback) {
-        window.setTimeout(callback, 1000 / 60);
-    };
-    
+var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+};
+
 function getElementTop(element) {
     var actualTop = element.offsetTop;
     var current = element.offsetParent;
@@ -223,6 +251,7 @@ function getElementLeft(element) {
     return actualLeft;
 }
 
+//getBoundingClientRect兼容，ie9之前坐标从-2开始的
 function getBoundingClientRect(element) {
     var scrollTop = document.documentElement.scrollTop;
     var scrollLeft = document.documentElement.scrollLeft;
@@ -262,5 +291,20 @@ function getBoundingClientRect(element) {
             top: actualTop - screenTop,
             bottom: actualTop + element.offsetHeight - scrollTop
         }
+    }
+}
+
+//hack matchesSelector
+function matchesSelector(element, selector) {
+    if (element.matchesSelector) {
+        return element.matchesSelector(selector);
+    } else if (element.webkitMatchesSelector) {
+        return element.webkitMatchesSelector(selector);
+    } else if (element.mozMatchesSelector) {
+        return element.mozMatchesSelector(selector);
+    } else if (element.msMatchesSelector) {
+        return element.msMatchesSelector(selector);
+    } else {
+        throw Error('not supported.');
     }
 }
