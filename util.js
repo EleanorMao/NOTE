ffa60ea1820fae7ef5727ac4cdb85beb608e504e
EleanorMao <danningmao@outlook.com>
2017-07-17 17:56:36 +0800

    Update util.js

--- util.js
+++ util.js
@@ -293,18 +293,3 @@ function getBoundingClientRect(element) {
         }
     }
 }
-
-//hack matchesSelector
-function matchesSelector(element, selector) {
-    if (element.matchesSelector) {
-        return element.matchesSelector(selector);
-    } else if (element.webkitMatchesSelector) {
-        return element.webkitMatchesSelector(selector);
-    } else if (element.mozMatchesSelector) {
-        return element.mozMatchesSelector(selector);
-    } else if (element.msMatchesSelector) {
-        return element.msMatchesSelector(selector);
-    } else {
-        throw Error('not supported.');
-    }
-}
